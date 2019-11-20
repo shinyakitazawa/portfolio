383f08ac5281c8264ae28f07e9298819c0465787
my_name <my_name@example.com>
2019-11-20 20:46:43 +0900

    20191120

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 (window.onload = function() {
     'use strict'
-
+	// ハンバーガメニュークリック処理
 	var hamburger = document.getElementById('humbMenu');
 	var navBar = document.getElementById('nav-bar');
 	hamburger.addEventListener('click', () => {
 		navBar.classList.toggle('hidden');
 	});
+	
+	// ポートフォリオクリック時
+	var arts = document.getElementsByClassName('portfolio__art');
+	arts = Array.prototype.slice.call(arts);
+	arts.forEach(art => {
+		art.addEventListener('click', (e) => {
+			alert(art.className);
+		});
+	});
+	
 })();
